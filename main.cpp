8979d67d5d2d25aab20932955239fa19ad48f796
Chargonium <svenvandoorn@icloud.com>
2025-02-10 00:25:31 +0100

    Initial Commit (im so cool btw)

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <thread>
+#include <windows.h>
+
+
+void hideCursor() {
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+
+    GetConsoleCursorInfo(consoleHandle, &cursorInfo); // Get current cursor settings
+    cursorInfo.bVisible = FALSE;                      // Hide cursor
+    SetConsoleCursorInfo(consoleHandle, &cursorInfo); // Apply changes
+}
+
+void showCursor() {
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+
+    GetConsoleCursorInfo(consoleHandle, &cursorInfo);
+    cursorInfo.bVisible = TRUE; // Show cursor again
+    SetConsoleCursorInfo(consoleHandle, &cursorInfo);
+}
+
+void progressBar(int len) {
+    hideCursor();
+    for (int i = 1; i <= len; i++) {
+        char temp[len+1];
+        std::fill(temp, temp + i, '#');
+        std::fill(temp + i, temp + len, '=');
+        temp[len]='\0';
+        std::cout << '\r' << '[' << temp << ']' << std::flush;
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    }
+    std::cout << '\r' << std::endl;
+    showCursor();
+}
+
+int main() {
+    std::cout << "Oh my god, Look at this W progress bar!" << std::endl;
+    std::ios_base::sync_with_stdio(false); // Disable syncing with C stdio for performance
+    progressBar(100);
+    return 0;
+}
